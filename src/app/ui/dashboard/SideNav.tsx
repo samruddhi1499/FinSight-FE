8a14f27176c65025a66b5e4ad9eb56d8af288b06
samruddhi1499 <samchavanxiv9@gmail.com>
2025-10-03 11:07:39 -0400

    added Skeleton

--- src/app/ui/dashboard/SideNav.tsx
+++ src/app/ui/dashboard/SideNav.tsx
@@ -4,7 +4,6 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { cookies } from 'next/headers';
 import {
   UserIcon,
   ClockIcon,
@@ -40,8 +39,7 @@ export default function SideNav({ onboarding }: { onboarding: boolean }) {
           });
     
           if (!response.ok) throw new Error('Failed to update user');
-          const cookieStore = await cookies();
-          cookieStore.delete('yourCookieName');
+      
            setTimeout(() => {
   window.location.reload();
 }, 100);

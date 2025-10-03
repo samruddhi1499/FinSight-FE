bddd66608d60834c2673e5ca4f54cd1cad4ba639
samruddhi1499 <samchavanxiv9@gmail.com>
2025-10-03 13:07:02 -0400

    fixing skeleton

--- src/app/ui/dashboard/Skeleton.tsx
+++ src/app/ui/dashboard/Skeleton.tsx
@@ -4,14 +4,15 @@ const shimmer =
 export function CardSkeleton() {
   return (
     <div
-      className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
+      className={`relative overflow-hidden rounded-xl bg-gray-50 p-4 shadow-sm flex-1 ${shimmer}`}
+      style={{ minWidth: 200 }}
     >
-      <div className="flex p-4">
+      <div className="flex items-center space-x-2 p-2.5">
         <div className="h-5 w-5 rounded-md bg-gray-200" />
-        <div className="ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium" />
+        <div className="h-5 w-20 rounded-md bg-gray-200 text-sm font-medium" />
       </div>
-      <div className="flex items-center justify-center truncate rounded-xl bg-white px-4 py-8">
-        <div className="h-7 w-20 rounded-md bg-gray-200" />
+      <div className="rounded-xl bg-white px-4 py-8 text-center">
+        <div className="mx-auto h-7 w-32 rounded-md bg-gray-200" />
       </div>
     </div>
   );
@@ -19,14 +20,15 @@ export function CardSkeleton() {
 
 export function CardsSkeleton() {
   return (
-    <>
+    <div className="flex space-x-4">
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
-    </>
+    </div>
   );
 }
+
 
 export function RevenueChartSkeleton() {
   return (

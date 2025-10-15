16917e8d7e04a82afbc31a4231d99471e940d733
samruddhi1499 <samchavanxiv9@gmail.com>
2025-10-15 13:44:57 -0400

    adding FE changes

--- src/app/ui/dashboard/CardSection.tsx
+++ src/app/ui/dashboard/CardSection.tsx
@@ -31,13 +31,16 @@ return (
     {cardData.map(({ title, amount }) => {
       const IconComponent = CARD_icon[title];
       return (
+        
         <div key={title} className="rounded-xl text-stone-600 bg-gray-50 p-4 shadow-sm flex-1">
           <div className="flex p-2.5 items-center">
             {IconComponent && <IconComponent className="h-5 w-5 text-gray-700" />}
-            <h3 className="ml-2 text-sm font-medium">{title}</h3>
+            { (title == "Current Savings" && amount < 0) && (<h3 className="ml-2 text-sm font-medium">Exceded By</h3>)}
+            { amount > 0 &&  <h3 className="ml-2 text-sm font-medium">{title}</h3>}
           </div>
           <p className="truncate rounded-xl bg-white px-4 py-8 text-center text-2xl">
-            ${amount}
+             {(title == "Current Savings" && amount < 0) && `\$${Math.abs(amount)}`}
+             { amount > 0 && `\$${amount}` } 
           </p>
         </div>
       );
